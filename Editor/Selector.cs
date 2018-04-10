cd681e7892a93003b5956ac55d8feed7cc16ad02
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-04-10 12:23:16 -0300

    created basic structure

---
+++ Editor/Selector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Graphene.BehaviourTree
+{
+    static class Selector
+    {
+        [MenuItem("Packages/Graphene")]
+        static void OpenPackageDirectory()
+        {
+            var path = "Packages/BehaviourTree-upm/package.json";
+            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(path);
+            EditorGUIUtility.PingObject(Selection.activeObject);
+        }
+    }
+}
